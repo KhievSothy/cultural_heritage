d061e61a4276553049ab6c8ee74e077272820f40
khonsaret <khonsaret@gmail.com>
2024-11-28 11:39:58 +0700

    updated home

--- src/i18m/index.js
+++ src/i18m/index.js
@@ -3,7 +3,7 @@ import { createI18n } from 'vue-i18n'
 
 const messages = {
     en: {
-        home:"Home",
+        home:"Homesssss",
         historical:"Historical Sites",
         museum:"Museum",
         event:"Event",
